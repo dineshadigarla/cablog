e97ad1be86caeecfb6a452ae6c7b46531e01c7eb
dineshadigarla <dineshadigarla@gmail.com>
2017-12-25 16:30:13 +0530

    push 3

--- ca_culfest/settings.py
+++ ca_culfest/settings.py
@@ -127,7 +127,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
 
 AUTHENTICATION_BACKENDS = (
     'social_core.backends.facebook.FacebookOAuth2',
